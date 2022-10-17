3038afd9c7c84fb7528bf8231b40eec2936b7c5b
Akshay A S <74396547+appukuttan23@users.noreply.github.com>
2022-10-17 10:44:40 +0530

    Create Question5.cpp
    // https://codeforces.com/problemset/problem/231/A
    // A. Team

---
+++ Akshay/Question5.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int p, v, t;
+    int n;
+    int result = 0;
+    while (n--)
+    {
+        cin >> p >> v >> t;
+        
+        if(p+v+t > 2) {
+            result += 1;
+        }
+    }
+    cout << result;
+    return 0;
+}
